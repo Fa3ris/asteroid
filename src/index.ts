dc8e816eb2c6fdf7da99b4561d0c95e131fc15a3
FabriceT <fabrice.tsiava@artik-consulting.com>
2021-04-17 15:56:37 +0200

    first commit:
    - parcel
    - typescript
    - sass

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import './style.scss'
+
+console.log("hello world!");
+
+const canvas: HTMLCanvasElement = document.createElement('canvas');
+
+canvas.id     = "canvas";
+canvas.width  = 1224;
+canvas.height = 768;
+
+document.body.appendChild(canvas);
+
